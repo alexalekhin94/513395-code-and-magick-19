b99ba83e3dbeb3f7551ad81af0907069bd81386e
alexalekhin94 <alekhinalexandr.94@gmail.com>
2020-02-15 04:04:34 +0300

    Задание по учебному проекту 4

--- js/setup.js
+++ js/setup.js
@@ -1,5 +1,5 @@
 var userDialog = document.querySelector('.setup')
-userDialog.classList.remove('hidden');
+
 document.querySelector('.setup-similar').classList.remove('hidden');
 
 nameArray = ['Иван','Хуан Себастьян', 'Мария','Кристоф', 'Виктор','Юлия', 'Люпита','Вашингтон'];
@@ -9,8 +9,13 @@ colorArray = ['rgb(101, 137, 164)','rgb(241, 43, 107)','rgb(146, 100, 161)','rgb
 
 eyesColor = ['black','red','blue','yellow','green'];
 
+coatColorArray = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
 
-// Функция, которая генерирует случайэлемент массива
+fireballColorArray = ['#ee4830','#30a8ee','#5ce6c0','#e848d5','#e6e848']
+
+
+
+// Функция, которая генерирует случайный элемент массива
 
 function arrayRandElement(arr) {
   var rand = Math.floor(Math.random() * arr.length);
@@ -59,3 +64,104 @@ similarWizardList.appendChild(fragment)
 
 
 
+
+
+// Open setup window//
+var userPageIcon = document.querySelector('.setup-open');
+var userIcon = document. querySelector ('.setup-open-icon')
+
+
+userPageIcon.addEventListener("click", onUserDialogClick );
+userIcon.addEventListener('keydown', onIconEnterPress);
+
+
+
+function onUserDialogClick(evt){
+  userDialog.classList.remove('hidden')
+
+  document.addEventListener('keydown', function(evt){
+    if(evt.key === 'Escape'){
+    userDialog.classList.add('hidden');
+  }
+  })
+
+
+}
+
+
+function onIconEnterPress(evt){
+if(evt.key === "Enter"){
+  userDialog.classList.remove('hidden');
+}
+}
+
+
+var userDialogClose = userDialog.querySelector('.setup-close');
+userDialogClose.addEventListener("click", onUserDialogCloseClick )
+userDialogClose.addEventListener('keydown', onIconCloseEnterPress)
+
+function onUserDialogCloseClick(evt){
+  userDialog.classList.add('hidden')
+}
+
+function onIconCloseEnterPress(evt){
+if (evt.key === "Enter"){
+  userDialog.classList.add('hidden');
+}
+}
+
+
+var setupSubmitButton = document.querySelector('.setup-submit');
+
+// setupSubmitButton.addEventListener('keydown', onSetupSubmitClick);
+setupSubmitButton.addEventListener('submit', onSetupSubmitClick);
+// setupSubmitButton.addEventListener('click', onSetupSubmitClick);
+
+function onSetupSubmitClick(evt){
+  evt.preventDefault();
+
+
+if(evt.key = "Enter"){
+  userDialog.classList.add('hidden')
+  }
+}
+
+
+var wizardSetup= document.querySelector('.setup-player');
+var wizardCoat = wizardSetup.querySelector('.wizard-coat');
+var wizardEyes = wizardSetup.querySelector('.wizard-eyes');
+var wizardFireball = document.querySelector('.setup-fireball-wrap');
+
+
+wizardCoat.addEventListener('click', onWizardCoatClick);
+
+function onWizardCoatClick(){
+  //Правильно ли задавать переменую для изменения знаечния здесь или это нужно делать вне функции?
+  var coatlColorValue = wizardSetup.querySelector('input[name = coat-color]');
+  var randomCoatColor = arrayRandElement(coatColorArray);
+  wizardCoat.style.fill = randomCoatColor;
+  coatlColorValue.value = randomCoatColor;
+
+}
+
+wizardEyes.addEventListener('click', onWizardEyesClick)
+
+function onWizardEyesClick(){
+
+  var eyeslColorValue = wizardSetup.querySelector('input[name = eyes-color]');
+  var randomEyesColor = arrayRandElement(coatColorArray);
+  wizardEyes.style.fill = randomEyesColor;
+  eyeslColorValue.value = randomEyesColor;
+
+}
+
+
+wizardFireball.addEventListener('click', onFireballClick);
+function onFireballClick() {
+  var firebolColorValue = wizardFireball.querySelector('input[name = fireball-color]')
+  var randomFireBallColor = arrayRandElement(fireballColorArray);
+  wizardFireball.style.background = randomFireBallColor;
+
+
+  firebolColorValue.value = randomFireBallColor;
+}
